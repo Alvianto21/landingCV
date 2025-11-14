8b5519c477369d1c12c73447e410bf2da6488daa
Alvianto21 <alviantovinsensius@gmail.com>
2025-11-14 11:59:57 +0700

    index  the JSON input

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -35,14 +35,14 @@ class UserFactory extends Factory
             'address' => fake()->address(),
             'phone_number' => fake()->phoneNumber(),
             'work_experiences' => json_encode([
-                [
+                0 => [
                     'company' => fake()->company(),
                     'position' => fake()->jobTitle(),
                     'start_date' => fake()->date(),
                     'end_date' => fake()->date(),
                     'description' => fake()->paragraph()
                 ],
-                [
+                1 => [
                     'company' => fake()->company(),
                     'position' => fake()->jobTitle(),
                     'start_date' => fake()->date(),
@@ -51,14 +51,14 @@ class UserFactory extends Factory
                 ]
             ]),
             'educations' => json_encode([
-                [
+                0 => [
                     'institution' => fake()->company(),
                     'degree' => 'Bachelor of ' . fake()->word(),
                     'start_date' => fake()->date(),
                     'end_date' => fake()->date(),
                     'link' => fake()->url()
                 ],
-                [
+                1 => [
                     'institution' => fake()->company(),
                     'degree' => 'Master of ' . fake()->word(),
                     'start_date' => fake()->date(),
@@ -68,9 +68,18 @@ class UserFactory extends Factory
             ]),
             'skills' => implode(', ', fake()->words(5)),
             'sosial_links' => json_encode([
-                'linkedin' => fake()->url(),
-                'github' => fake()->url(),
-                'x' => fake()->url()
+                0 => [
+                    'platform' => 'linkedin',
+                    'url' => fake()->url()
+                ],
+                1 => [
+                    'platform' => 'gitbub',
+                    'url' => fake()->url()
+                ],
+                2 => [
+                    'platform' => 'x',
+                    'url' => fake()->url()
+                ]
             ]),
             'gender' => fake()->randomElement(['male', 'female']),
             'place_of_birth' => fake()->city(),
