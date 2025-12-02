8e862cd56e2fb8a9b25a5b3364aae860e7bb30a3
Alvianto21 <alviantovinsensius@gmail.com>
2025-12-02 11:18:51 +0700

    remember password feature

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,13 +1,13 @@
 <x-layouts.app>
 	<x-slot:title>{{ $title }}</x-slot:title>
 
-	<!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
+	
 	<section class="bg-gray-50 dark:bg-gray-900">
 		<div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 			<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 				<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+					<!-- Session Status -->
+					<x-auth-session-status class="mb-4" :status="session('status')" />
 					<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
 						Sign in to your account
 					</h1>
@@ -31,7 +31,7 @@
 									<label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
 								</div>
 							</div>
-							<a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+							<a href="{{ route('password.request') }}" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
 						</div>
 						<button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
 						<p class="text-sm font-light text-gray-500 dark:text-gray-400">
