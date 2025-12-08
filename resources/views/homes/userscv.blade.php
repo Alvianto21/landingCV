e792bd531ed92307d8d5563549b84703ccc43c61
Alvianto21 <alviantovinsensius@gmail.com>
2025-12-08 08:47:57 +0700

    fix typo in img search adn add login link

--- resources/views/homes/userscv.blade.php
+++ resources/views/homes/userscv.blade.php
@@ -4,7 +4,7 @@
     <!-- Profile -->
 	<section class="bg-gray-200 dark:bg-gray-700">
 		<div class="py-8 px-4 mx-auto max-w-screen-xl text-center justify-items-center lg:py-16">
-			<img src="{{ asset('storage/' . $user->photo_profile) }}" alt="{{ $user->username }}" width="300" class="rounded-circle">
+			<img src="{{ asset('storage/' . $user->profile_picture) }}" alt="{{ $user->username }}" width="300" class="rounded-circle">
 			<h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-5xl dark:text-white">{{ $user->name }}</h1>
 			<p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">{{ $user->place_of_birth }}, {{ Carbon\Carbon::parse($user->date_of_birth)->format('d-m-Y') }}</p>
 		</div>
